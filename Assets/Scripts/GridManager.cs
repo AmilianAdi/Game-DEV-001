6f38d7467232cb507d46050c33a96e365802ddcf
prg23hxu <153437289+AmilianAdi@users.noreply.github.com>
2026-02-25 19:41:37 +0000

    Grid attempt

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public static GridManager Instance;
+    private Dictionary<Vector3Int, GameObject> 
+        grid = new Dictionary<Vector3Int, GameObject>();
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void RegisterEntity(GameObject entity, Vector3Int gridPos)
+    {
+        if (!grid.ContainsKey(gridPos))
+            grid.Add(gridPos, entity);
+    }
+    public void UnregisterEntity(Vector3Int oldPos)
+        //unregister when move off
+    {
+        if (grid.ContainsKey(oldPos))
+            grid.Remove(oldPos);
+    }
+    public bool IsTileOccupied(Vector3Int gridPos)
+    //TILE OCCUPIED
+    {
+        return grid.ContainsKey(gridPos);
+    }
+    public GameObject GetEntityAt(Vector3Int gridPos)
+    {
+        grid.TryGetValue(gridPos, out GameObject entity);
+        return entity;
+    }
+}
